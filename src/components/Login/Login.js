273b3a7f7b747892ebb6f4b46c571cb334036856
rakibul-cse-diu <rakibul2180@gmail.com>
2022-04-18 16:10:34 +0600

    add login with email and password auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
 import googleLogo from '../../images/Google-logo.png';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+
+    if (error) {
+        return (
+            <div className='min-h-screen'>
+                <p>Error: {error.message}</p>
+            </div>
+        );
+    }
+    if (loading) {
+        return <div className='min-h-screen'><p>Loading...</p></div>;
+    }
+    if (user) {
+        return (
+            <div className='min-h-screen'>
+                <p>Signed In User: {user.email}</p>
+            </div>
+        );
+    }
+
+
+
+    const handleEmail = (event) => {
+        const value = event.target.value;
+        setEmail(value);
+    }
+    const handlePassword = (event) => {
+        const value = event.target.value;
+        setPassword(value);
+    }
+    const handleLogin = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password)
+
+    }
+
     return (
         <div className='min-h-screen flex flex-col justify-center items-center'>
             <h2 className='text-lg font-medium'>Login with email and password</h2>
-            <form className='flex flex-col w-3/4 md:w-1/4'>
+            <form onSubmit={handleLogin} className='flex flex-col w-3/4 md:w-1/4'>
                 <label className='text-lg mt-5' htmlFor="email">Email</label>
-                <input className='h-10 pl-3 outline-[#1F2B6C] border-[1px] border-gray-200' type="email" name="email" id="email" placeholder='Enter your email' required />
+                <input onBlur={handleEmail} className='h-10 pl-3 outline-[#1F2B6C] border-[1px] border-gray-200' type="email" name="email" id="email" placeholder='Enter your email' required />
                 <label className='text-lg mt-3' htmlFor="password">Password</label>
-                <input className='h-10 pl-3 outline-[#1F2B6C] border-[1px] border-gray-200' type="password" name="password" id="password" placeholder='Enter your password' required />
+                <input onBlur={handlePassword} className='h-10 pl-3 outline-[#1F2B6C] border-[1px] border-gray-200' type="password" name="password" id="password" placeholder='Enter your password' required />
                 <input className='my-5 px-6 py-1 bg-[#1F2B6C] text-white text-lg font-semibold cursor-pointer' type="submit" value="Login" />
                 <Link to="/resetpassword"><span className='text-sm text-blue-500 underline decoration-blue-500'>Forgot password?</span></Link>
                 <span className='text-sm'>Don't have any account? <Link to="/register"><button className='text-blue-500 underline decoration-blue-500'>register</button></Link></span>
