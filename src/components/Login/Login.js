1c68b62c85f0f5a09127820345b0ff3af637b59a
rakibul-cse-diu <rakibul2180@gmail.com>
2022-04-18 10:31:28 +0600

    add social login button on login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import googleLogo from '../../images/Google-logo.png';
 
 const Login = () => {
     return (
@@ -14,6 +15,19 @@ const Login = () => {
                 <Link to="/resetpassword"><span className='text-sm text-blue-500 underline decoration-blue-500'>Forgot password?</span></Link>
                 <span className='text-sm'>Don't have any account? <Link to="/register"><button className='text-blue-500 underline decoration-blue-500'>register</button></Link></span>
             </form>
+            <div className='mt-8'>
+                <div className='flex justify-center items-center'>
+                    <div className='w-[80px] h-[3px] bg-slate-200'></div>
+                    <small className='mx-2 text-lg font-semibold'>Or</small>
+                    <div className='w-[80px] h-[3px] bg-slate-200'></div>
+                </div>
+                <div>
+                    <button className='my-5 px-6 py-1 bg-[#BFD2F8] text-[#1F2B6C] hover:bg-[#1F2B6C] hover:text-[#BFD2F8] duration-500 text-lg font-semibold cursor-pointer rounded-sm'>
+                        <img className='inline mr-3' src={googleLogo} alt="" height={30} width={30} />
+                        Login with Google
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
