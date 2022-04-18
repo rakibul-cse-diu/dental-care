b7ccb26af5b9517a9990edc9c01b72037e3765fc
rakibul-cse-diu <rakibul2180@gmail.com>
2022-04-18 17:14:26 +0600

    add reset password send  email

--- src/components/ForgotPass/ForgotPass.js
+++ src/components/ForgotPass/ForgotPass.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const ForgotPass = () => {
+    const [email, setEmail] = useState('');
+    const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(
+        auth
+    );
+
+
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error.message}</p>
+            </div>
+        );
+    }
+    if (sending) {
+        return <p>Sending...</p>;
+    }
+
+    const handleEmail = (event) => {
+        const value = event.target.value;
+        setEmail(value);
+    }
+
+    const handleReset = () => {
+        sendPasswordResetEmail(email);
+        alert('Sent email');
+    }
+
     return (
         <div className='min-h-screen flex flex-col justify-center items-center'>
             <h2 className='text-lg font-medium'>Login with email and password</h2>
             <form className='flex flex-col w-3/4 md:w-1/4'>
                 <label className='text-lg mt-5' htmlFor="email">Email</label>
-                <input className='h-10 pl-3 outline-[#1F2B6C] border-[1px] border-gray-200' type="email" name="email" id="email" placeholder='Enter your email' required />
-                <input className='my-5 px-6 py-1 bg-[#1F2B6C] text-white text-lg font-semibold cursor-pointer' type="submit" value="Reset" />
+                <input onBlur={handleEmail} className='h-10 pl-3 outline-[#1F2B6C] border-[1px] border-gray-200' type="email" name="email" id="email" placeholder='Enter your email' required />
+                <button onClick={handleReset} className='my-5 px-6 py-1 bg-[#1F2B6C] text-white text-lg font-semibold cursor-pointer'>Reset</button>
             </form>
         </div>
     );
